da2708a5814711d153c5893b6510843b8c1977ca
Abdullah Ali Saleem <Abdual@metropolia.fi>
2024-11-05 23:11:07 +0200

    this is JV

---
+++ Module 1/mod1.js
@@ -0,0 +1,55 @@
+/*
+Module 1.1
+
+console.log("I'm printing to console!")
+ */
+/*
+Module 1.2
+
+let p1 = prompt("Enter your name ");
+object = document.getElementById("target0")
+object.innerHTML = "Moi, " + p1 + "!!";
+
+ */
+/* Module 1.3
+
+let x = parseInt(prompt("Enter Integer Number 1"));
+let y = parseInt(prompt("Enter Integer Number 2"));
+let z = parseInt(prompt("Enter Integer Number 3"));
+sum = x + y + z
+avg = sum / 3
+prdct = x * y * z
+
+document.querySelector('#target1').innerHTML  = ("this is the sum of the Numbers: " + sum)
+document.querySelector('#target2').innerHTML  = ("this is the average of the numbers: " + avg)
+document.querySelector('#target3').innerHTML  = ("this is the product of the numbers: "  + prdct)
+*/
+/* Module 1.4
+
+function studentsorting() {
+  const name = prompt("Hey there!!, Enter your name.");
+
+  const housenum = Math.floor(Math.random() * 4)+1;
+  let house;
+  switch (housenum) {
+    case 1:
+      house = "GRYFFINDOR";
+      break;
+    case 2:
+      house = "SLYTHERIN";
+      break;
+    case 3:
+      house = "HUFFLEPUFF";
+      break;
+    case 4:
+      house = "RAVENCLAW";
+      break;
+    default:
+      house = "NO HOUSE ASSIGNED";
+
+  }
+  document.getElementById('target4').textContent = `${name}, CONGRATS, YOU ARE ${house}.`;
+}
+studentsorting()
+
+ */
